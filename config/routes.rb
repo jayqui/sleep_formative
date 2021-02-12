8fe094596a5926ecd59f8eee73fcd35f737bf737
Jay Quigley <quigley.james.g@gmail.com>
2021-02-12 08:24:03 -0800

    make diary_entries#index the default path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :diary_entries, only: [:index, :show, :new, :create]
+  root to: 'diary_entries#index'
+  resources :diary_entries, only: %i[index show new create]
 end
